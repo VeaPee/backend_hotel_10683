537b4cc51362bf5c20a7611d1928124a199d8636
Vincentius Agung Prabandaru <vap.agung@gmail.com>
2023-10-20 20:06:51 +0700

    CORS

--- app.js
+++ app.js
@@ -35,6 +35,14 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.status(200).end();
+    return;
+  }
+  
   next();
 });
 
