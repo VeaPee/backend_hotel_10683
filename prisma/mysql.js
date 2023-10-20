a5924733c642ed97aaf6a0e23cd82f1843425644
Vincentius Agung Prabandaru <vap.agung@gmail.com>
2023-10-20 16:06:56 +0700

    teststst

---
+++ prisma/mysql.js
@@ -0,0 +1,20 @@
+const { SecretManagerServiceClient } = require('@google-cloud/secret-manager');
+const secretmanagerClient = new SecretManagerServiceClient();
+
+const callAccessSecretVersion = async () => {
+  // Construct request
+  const request = {
+    name: 'projects/999454011714/secrets/MYSQL/versions/latest',
+  };
+
+  // Run request
+  const [response] = await secretmanagerClient.accessSecretVersion(request);
+  const secretValue = response.payload.data.toString();
+
+  // Set secretValue in the environment
+  process.env.SECRET_VALUE = secretValue;
+
+  return secretValue;
+};
+
+module.exports = callAccessSecretVersion;
